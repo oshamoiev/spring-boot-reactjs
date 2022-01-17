477b56c9f30eb5e134fd5b0c13952b1f3807e9c8
Oleksandr Shamoiev <wamoev@gmail.com>
2022-01-17 22:14:21 +0200

    Generate Spring Boot app

---
+++ src/test/java/com/learn/springbootreactjs/SpringBootReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springbootreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
